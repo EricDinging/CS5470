0f4ad5e1e0fe7a13e484f3021cb5d5ec4b8b6e1c
EricDinging <dd687@cornell.edu>
2025-08-28 07:53:18 -0700

    Test different request rate

--- assignment_1/client.sh
+++ assignment_1/client.sh
@@ -8,10 +8,18 @@
 #     --long-prompts 0 \
 #     --long-prompt-len 32000
 
+# python3 benchmark.py --backend vllm \
+#     --model meta-llama/Llama-3.1-8B \
+#     --request-rate 2 \
+#     --num-prompts 25 \
+#     --dataset-name dummy \
+#     --long-prompts 0 \
+#     --long-prompt-len 32000
+
 python3 benchmark.py --backend vllm \
     --model meta-llama/Llama-3.1-8B \
-    --request-rate 2 \
-    --num-prompts 25 \
+    --request-rate 10 \
+    --num-prompts 100 \
     --dataset-name dummy \
     --long-prompts 0 \
     --long-prompt-len 32000
--- assignment_1/server.sh
+++ assignment_1/server.sh
@@ -10,6 +10,27 @@
 #     --max-num-seqs 512 \
 #     --disable-sliding-window
 
+# CUDA_VISIBLE_DEVICES=0 python3 -m vllm.entrypoints.openai.api_server \
+#     --model meta-llama/Llama-3.1-8B \
+#     --swap-space 16 \
+#     --disable-log-requests \
+#     --enforce-eager \
+#     --enable-chunked-prefill \
+#     --max-num-batched-tokens 512 \
+#     --max-num-seqs 512 \
+#     --disable-sliding-window
+
+# CUDA_VISIBLE_DEVICES=0,1 python3 -m vllm.entrypoints.openai.api_server \
+#     --model meta-llama/Llama-3.1-8B \
+#     --swap-space 16 \
+#     --disable-log-requests \
+#     --enforce-eager \
+#     --enable-chunked-prefill \
+#     --max-num-batched-tokens 512 \
+#     --max-num-seqs 512 \
+#     --disable-sliding-window \
+#     --tensor-parallel-size 2
+
 CUDA_VISIBLE_DEVICES=0,1,2,3 python3 -m vllm.entrypoints.openai.api_server \
     --model meta-llama/Llama-3.1-8B \
     --swap-space 16 \
